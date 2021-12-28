7c6c15eb3d7ab4ebb624367dde172c27a2eb162d
Omer Gimenez Llach <omer.gimenez@gmail.com>
2021-12-28 15:41:34 -0800

    Add \[\]-tracking capabilities into unfill.
    The code gets uglier, I will fix it if I find
    more stuff to track.

--- translations/ca/unfill.cc
+++ translations/ca/unfill.cc
@@ -27,6 +27,26 @@ int main() {
       mem[++count] = line;
       cout << "[[[[" << count << "]]]]" << "\n";
       line.clear();
+    } else if (s == "\\[") {
+      string end = "\\]";
+      cout << line << "\n";
+      line.clear();
+      line = s + "\n";
+      while (getline(cin, s)) {
+        line += s + "\n";
+        if (s == end) {
+          break;
+        }
+      }
+      mem[++count] = line;
+      cout << "[[[[" << count << "]]]]" << "\n";
+      line.clear();
+    } else if (s.substr(0,2) == "\\[" and s.substr(s.size()-2, 2) == "\\]") {
+      cerr << "Found single-line \\[ \\]" << "\n";
+      cout << line << "\n";
+      mem[++count] = s + "\n";
+      cout << "[[[[" << count << "]]]]" << "\n";
+      line.clear();
     } else {
       if (line != "" && !s.empty()) line += " ";
       line += s;
